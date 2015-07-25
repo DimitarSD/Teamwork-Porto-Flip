9415535eb514ed19a79cbfd971fe0406eba7df22
BiserSirakov <biser.sirakov@gmail.com>
2015-07-25 10:48:49 +0300

    Added test music background + fixing the merge

--- Game/scripts/game-core.js
+++ Game/scripts/game-core.js
@@ -24,9 +24,16 @@
         game.load.image('loading', 'assets/loading.png');
         game.load.image('load_progress_bar', 'assets/progress_bar_bg.png');
         game.load.image('load_progress_bar_dark', 'assets/progress_bar_fg.png');
+
+        game.load.audio('bg-music', 'music/kickstarter.mp3');
     };
 
     BootState.prototype.create = function () {
+        var music = game.add.audio('bg-music', 1, true);
+        music.autoplay = true;
+
+        music.play();
+
         game.state.start('load');
     };
 
