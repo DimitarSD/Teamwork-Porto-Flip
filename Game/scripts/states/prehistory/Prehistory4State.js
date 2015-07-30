4c8fa8c2d636b073530a157c05e6a28f509d662b
tarlit <tarlitan@gmail.com>
2015-07-30 09:32:26 +0300

    New PrehistoryState files, main.js - modified, new image, new file prehistory.css

---
+++ Game/scripts/states/prehistory/Prehistory4State.js
@@ -0,0 +1,53 @@
+define(['game'], function (game) {
+    var body = document.getElementsByTagName('body')[0],
+        div = document.createElement('div');
+
+    function Prehistory4State() {
+    };
+
+    Prehistory4State.prototype.create = function () {
+        var span,
+            spanText,
+            divPrehistory,
+            divPrehistoryText,
+            playGameButton,
+            playGameButtonText,
+            playButton;
+
+        span = document.createElement('span');
+        spanText = document.createTextNode('Prehistory');
+        span.textContent = spanText.textContent;
+
+        divPrehistory = document.CreateElement('div');
+        divPrehistoryText = document.createTextNode('Once upon a time ...');
+        divPrehistory.textContent = divPrehistoryText.textContent;
+        divPrehistory.id = 'divPrehistory';
+        divPrehistory.style.backgroundImage = "url('images/Prehistory/paper-roll.png')";     
+
+        playGameButton = document.createElement('button');
+        playGameButton.className = 'hvr-border-fade';
+        playGameButton.id = 'play-game';
+
+        playGameButtonText = document.createTextNode('Play Now');
+        playGameButton.textContent = playGameButtonText.textContent;
+
+        div.appendChild(span);
+        div.appendChild(divPrehistory);
+        div.appendChild(playGameButton);
+
+        div.style.backgroundImage = "url('images/Game Over - messages background/L4-bigBird.jpg')";
+
+        body.appendChild(div);
+
+        playButton = document.getElementById('play-game');
+        playButton.onclick = function () {
+            div.removeChild(span);
+            div.removeChild(divPrehistory);
+            div.removeChild(playGameButton);
+            body.removeChild(div);
+            game.state.start('play');
+        };
+    };
+
+    return Prehistory4State;
+});
