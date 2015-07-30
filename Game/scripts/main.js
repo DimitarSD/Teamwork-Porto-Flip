4c9a69de0ce1a8c734bf2c56ae770e285f73060e
Gergana Belcheva <gergana.belcheva@gmail.com>
2015-07-30 11:58:13 +0300

    level 2

--- Game/scripts/main.js
+++ Game/scripts/main.js
@@ -1,9 +1,9 @@
 define(['game', 'states/BootState', 'states/LoadState', 'states/MenuState', 'states/PlayState',
-        'states/HowToState', 'states/CreditsState', 'states/GameOverState','states/levels/Level1State', 'states/levels/Level3State', 'states/levels/Level4State',
+        'states/HowToState', 'states/CreditsState', 'states/GameOverState','states/levels/Level1State', 'states/levels/Level2State', 'states/levels/Level3State', 'states/levels/Level4State',
         'states/prehistory/Prehistory1State', 'states/prehistory/Prehistory2State',
         'states/prehistory/Prehistory3State', 'states/prehistory/Prehistory4State'],
     function (game, BootState, LoadState, MenuState, PlayState, HowToState, CreditsState, GameOverState,
-              Level1State, Level3State, Level4State, Prehistory1State, Prehistory2State, Prehistory3State, Prehistory4State) {
+              Level1State, Level2State, Level3State, Level4State, Prehistory1State, Prehistory2State, Prehistory3State, Prehistory4State) {
         //game.state.add('ninja-flipper', PlayState, true);
 
         game.state.add('boot', BootState);
@@ -13,6 +13,7 @@ define(['game', 'states/BootState', 'states/LoadState', 'states/MenuState', 'sta
         game.state.add('howto', HowToState);
         game.state.add('credits', CreditsState);
         game.state.add('level1', Level1State);
+        game.state.add('level2', Level2State);
         game.state.add('level3', Level3State);
         game.state.add('level4', Level4State);
         game.state.add('prehistory1', Prehistory1State);
